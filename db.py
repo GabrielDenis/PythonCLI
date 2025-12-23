4412657e831fafdacdc3ad8935cac7bfabcd7ffc
GabrielDenis <83370739+GabrielDenis@users.noreply.github.com>
2025-12-23 16:22:52 -0300

    feat(db) create books table with foreign key

--- db.py
+++ db.py
@@ -8,19 +8,20 @@ def get_connection():
         host="localhost"
     )
 
-def create_table():
+def run_migrations():
     conn = get_connection()
     cursor = conn.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS topics (id SERIAL PRIMARY KEY,name VARCHAR(255) NOT NULL, status VARCHAR(255) NOT NULL)")
     cursor.execute("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY,username VARCHAR(50) UNIQUE NOT NULL,password_hash VARCHAR(255) NOT NULL)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS books (id SERIAL PRIMARY KEY, title VARCHAR(255) NOT NULL, author VARCHAR(255), topic_id INTEGER REFERENCES topics(id) ON DELETE CASCADE)")
     conn.commit()
     cursor.close()
     conn.close()
 
-    print("Table 'topics' created successfully!")
+    print("Migrations run successfully!")
 
 if __name__ == "__main__":
     try:
-        create_table()
+        run_migrations()
     except Exception as e:
         print(f"Error: {e}")
