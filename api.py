3489543d39a295c313899a03fe1290408af1ccd5
GabrielDenis <83370739+GabrielDenis@users.noreply.github.com>
2025-12-25 16:48:31 -0300

    feat(api): Add OpenLibrary integration

--- api.py
+++ api.py
@@ -5,6 +5,9 @@ from datetime import datetime, timedelta
 from jose import jwt
 from dotenv import load_dotenv
 import db
+import cache
+import json
+import httpx
 from os import getenv
 
 load_dotenv()
@@ -21,6 +24,10 @@ class Topic(BaseModel):
 class UserCreate(BaseModel):
     username: str
     password: str
+
+class Book(BaseModel):
+    title: str
+    author: str
 
 def get_password_hash(password: str):
     return pwd_context.hash(password)
@@ -41,6 +48,13 @@ def read_root():
 
 @app.get("/topics")
 def read_topics():
+    try:
+        cached_topics = cache.get_cache("all_topics")
+        if cached_topics:
+            print("Cache Hit")
+            return json.loads(cached_topics)
+    except Exception as e:
+        print(f"Redis not ready: {e}")
     conn = db.get_connection()
     cur = conn.cursor()
     cur.execute("SELECT * FROM topics")
@@ -57,6 +71,11 @@ def read_topics():
             "status": row[2]
         })
 
+    try:
+        cache.set_cache("all_topics", topics_data)
+    except:
+        pass
+
     return topics_data
 
 @app.post("/topics")
@@ -68,6 +87,11 @@ def create_topic(topic: Topic):
     cur.close()
     conn.close()
 
+    try:
+        cache.r.delete("all_topics")
+    except:
+        pass
+
     return {"message": "Topic created successfully"}
 
 @app.put("/topics/{topic_name}/done")
@@ -78,6 +102,11 @@ def mark_done(topic_name: str):
     conn.commit()
     cur.close()
     conn.close()
+
+    try:
+        cache.r.delete("all_topics")
+    except:
+        pass
 
     return {"message": "Topic marked as done successfully"}
 
@@ -109,3 +138,35 @@ def login(user: UserCreate):
 
     access_token = create_access_token(data={"sub": user.username})
     return {"access_token": access_token, "token_type": "bearer"}
+
+@app.post("/topics/{topic_id}/enrich")
+async def enrich_topics(topic_id: int):
+    conn = db.get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT name FROM topics WHERE id = %s", (topic_id,))
+    topic = cur.fetchone()
+
+    if not topic:
+        raise HTTPException(status_code=404, detail="Topic not found")
+    
+    topic_name = topic[0]
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f"https://openlibrary.org/search.json?q={topic_name}")
+        data = response.json()
+
+    saved_books = []
+
+    for doc in data['docs'][:3]:
+        title = doc.get('title', 'Unknown')
+        author = doc.get('author_name', ['Unknown'])[0]
+
+        cur.execute("INSERT INTO books (title, author, topic_id) VALUES (%s, %s, %s)", (title, author, topic_id))
+        saved_books.append({"title": title, "author": author})
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return {"message": "Books Found!", "books": saved_books}
+            
